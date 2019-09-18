430b8dcdc9c0e5aad098c97cb9166d8f74a1bc0b
dududu04 <1219891914@qq.com>
2019-09-19 04:59:55 +0800

    add epoll

--- web服务器/多任务并发的web服务器/06-epoll实现http.py
+++ web服务器/多任务并发的web服务器/06-epoll实现http.py
@@ -1,16 +1,14 @@
 import socket
 import re
+import select
 
 
 def service_client(new_socket, request):
 	"""为这个客户端返回数据"""
-	
-	# 切割请求为一个列表
 	request_lines = request.splitlines()	
 	print("")
 	print(">>>>>"*20)
 	print(request_lines)
-	# 提取请求的文件名 GET /index.html HTTP/1.1 get/post/put/del
 	ret = re.match(r"[^/]+(/[^ ]*)", request_lines[0])
 	if ret:
 		file_name = ret.group(1)  # 请求"127.0.0.1:7890/index.html" file_name为"/index.html"
@@ -28,60 +26,44 @@ def service_client(new_socket, request):
 	else:
 		html_content = f.read()
 		f.close()
-		# 准备发送给浏览器的数据 --- body
 		response_body = html_content
-		# 准备发送给浏览器的数据 --- header
 		response_header = "HTTP/1.1 200 OK\r\n"
 		response_header += "Content-Length:%d\r\n" % len(response_body)
 		response_header += "\r\n"
 		response = response_header.encode("utf-8") + response_body
-		# 将response发送
 		new_socket.send(response)
 
 
 
 def main():
 	"""用来完成整体的控制"""
-	# 创建套接字
 	tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	# 设置当服务器先close即服务器4次挥手之后资源能够立即释放，这样就保证了下次运行程序时，可以立即执行
 	tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)	
-
-	# 绑定
 	tcp_server_socket.bind(("",7890))
-
-	# 变为监听套接字
 	tcp_server_socket.listen(128)
-	
-	# 将套接字变为非阻塞
 	tcp_server_socket.setblocking(False)
-
-	client_socket_list = list()
+	# 创建一个epoll对象
+	epl = select.epoll()	
+	# 将监听套接字对应的fd注册到epoll中
+	epl.register(tcp_server_socket.fileno(), select.EPOLLIN)
+	fd_event_dict = dict()
 	while True:
-		try:
-			# 等待新客户端的链接
-			new_socket, client_addr = tcp_server_socket.accept()
-			
-		except Exception as ret:
-			pass
-		else:			
-			new_socket.setblocking(False)
-			client_socket_list.append(new_socket)
-
-		for client_socket in client_socket_list:
-			try:
-				recv_data = client_socket.recv(1024).decode("utf-8")
-			except Exception as ret:
-				pass
-			else:
+		fd_event_list = epl.poll()  # 默认会阻塞，直到 os监测到数据的到来 通过事件通知的方式 告诉这个程序，此时才会解阻塞
+		# fd_event_list = [(fd,event),(套接字对于的文件描述符，这个文件描述符到底是什么事件 例如 可以调用recv接收等)]
+		for fd,event in fd_event_list:
+			if fd == tcp_server_socket.fileno():
+				# 创建新的套接字
+				new_socket, client_addr = tcp_server_socket.accept()
+				epl.register(new_socket.fileno(), select.EPOLLIN)
+				fd_event_dict[new_socket.fileno()] = new_socket
+			elif event == select.EPOLLIN:
+				recv_data = fd_event_dict[fd].recv(1024).decode("utf-8")
 				if recv_data:
-					service_client(client_socket, recv_data)
+					service_client(fd_event_dict[fd], recv_data)
 				else:
-					client_socket_list.remove(client_socket)
-					client_socket.close()
-					
-
+					fd_event_dict[fd].close()
+					epl.unregister(fd)
+					del fd_event_dict[fd]
 	# 关闭监听套接字
 	tcp_server_socket.close()
 
